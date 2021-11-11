6c770ef24cccbd29d7e972c12c3ed5c853856560
YZ_lxh <qtj-3gw3gf3ft@dingtalk.com>
2021-11-11 10:28:29 +0800

    Initial Commit

---
+++ HeaderImageAutoAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HeaderImageAutoAnimation
+//
+//  Created by YZ-LXH on 2021/11/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
