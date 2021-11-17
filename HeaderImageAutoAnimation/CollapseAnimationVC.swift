59dfedb9f463cbf1ba8744ac20dbecc8f978c2a7
YZ_lxh <qtj-3gw3gf3ft@dingtalk.com>
2021-11-17 11:08:32 +0800

    first commit

---
+++ HeaderImageAutoAnimation/CollapseAnimationVC.swift
@@ -0,0 +1,29 @@
+//
+//  CollapseAnimationVC.swift
+//  HeaderImageAutoAnimation
+//
+//  Created by YZ-LXH on 2021/11/11.
+//
+
+import UIKit
+
+class CollapseAnimationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HeaderImageAutoAnimation/CommonInfo.swift
@@ -0,0 +1,8 @@
+//
+//  CommonInfo.swift
+//  HeaderImageAutoAnimation
+//
+//  Created by YZ-LXH on 2021/11/11.
+//
+
+import Foundation
